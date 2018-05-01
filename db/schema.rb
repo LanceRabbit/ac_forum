27b439ad651d93735c0aa8d5f698a51cbcca3ef9
LanceRabbit <lancerabbit@gmail.com>
2018-05-01 17:17:31 +0800

    create collection model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180501045026) do
+ActiveRecord::Schema.define(version: 20180501055119) do
+
+  create_table "collections", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id", "user_id"], name: "index_collections_on_post_id_and_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
