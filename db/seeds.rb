bb6f05a302555f1b6adbe1d74d25cf2e7a9482c9
LanceRabbit <lancerabbit@gmail.com>
2018-05-02 11:41:55 +0800

    add admin user and 3 categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+
+category_list =[
+  { name: "商業類" },
+  { name: "語文類" },
+  { name: "工程類" }
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name] )
+end
+puts "Category created!"
+
+User.destroy_all
+User.create(email: "admin@example.com", name: "root" , password:"12345678", role:"admin")
+puts " #{User.count} users data"
+
+puts "Default admin created!"
