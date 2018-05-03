52fa8ae8b78d5b6181c0d796d0c34f30b89f4f99
LanceRabbit <lancerabbit@gmail.com>
2018-05-03 14:22:29 +0800

    post can upload file via imageUploader

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates_presence_of :title, :content
+  
+  #上傳圖檔
+  mount_uploader :image, ImageUploader
+
+  belongs_to :user
+
+  has_many :post_categories
+  has_many :categories, through: :post_categories
+  
+
+
 end
